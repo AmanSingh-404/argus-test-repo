29b41d870e5303bec6c99bdae17b0bc0fbe73d8d
Aman Singh <amanscode2005@gmail.com>
2026-07-19 18:56:23 +0530

    Allow fetching deleted users in get_user function
    Removed condition to exclude deleted users from query.

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 def get_user(user_id, include_deleted=False):
     """Fetches a user record by ID from the database."""
     query = "SELECT * FROM users WHERE id = ?"
-    
-    if not include_deleted:
-        query += " AND deleted_at IS NULL"
+  
     
     result = db.execute(query, (user_id,))
     return result if result else None
